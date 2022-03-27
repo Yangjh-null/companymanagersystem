ad56f9eb86b1c687714242514966b61b378e436d
yangjunhua <yangjh_null@sian.com>
2022-03-27 22:02:16 +0800

    companyManagerDemo first-commit

---
+++ src/main/java/com/companymanager/entity/condition/TransInfoSumCondition.java
@@ -0,0 +1,17 @@
+package com.companymanager.entity.condition;
+
+import com.companymanager.entity.TransactionInfoSum;
+
+public class TransInfoSumCondition extends TransactionInfoSum {
+
+    private String empName;
+
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+}
